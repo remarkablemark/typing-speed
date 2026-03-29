2fa7b2809934c5f290067a5ab6a3835ec27c3ec4
Mark <mark@remarkablemark.org>
2026-03-29 16:55:00 -0400

    refactor: tidy style.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import './index.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 import { TypingTestApp } from './components/TypingTestApp';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App>
       <TypingTestApp />
